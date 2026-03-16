1f2face53167e348b50b2d2607e39b87a5888440
raginirai14 <raginirai1415@gmail.com>
2026-03-16 19:04:21 +0530

    Initial Commit

---
+++ SafeHer/SafeHerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeHerApp.swift
+//  SafeHer
+//
+//  Created by Ragini Rai on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SafeHerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
